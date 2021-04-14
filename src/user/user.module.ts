735eaae70b9ed0fde6c5414440df8adba3670dea
jinwoo <jintakk6531@gmail.com>
2021-04-14 21:23:10 +0900

    4/14 db연동 완료

---
+++ src/user/user.module.ts
@@ -0,0 +1,10 @@
+import { User } from './models/user.entity';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UserController],
+})
+export class UserModule {}
