82b428f101cba06bbd07c24209b8088be67505a7
jinwoo <jintakk6531@gmail.com>
2021-04-19 21:53:12 +0900

    4/19 bcrypt와 bcryptjs 차이학습

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { UserService } from './../user/user.service';
 import { Body, Controller, Post } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @Controller()
 export class AuthController {
@@ -7,6 +8,12 @@ export class AuthController {
 
   @Post('register')
   async register(@Body() body) {
-    return this.userService.create(body);
+    const hashed = await bcrypt.hash(body.password, 12);
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: hashed,
+    });
   }
 }
