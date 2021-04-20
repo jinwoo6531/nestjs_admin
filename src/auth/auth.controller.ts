acfd497a20da3b6974567374c45314025ed7c0fc
jinwoo6531 <jintakk6531@gmail.com>
2021-04-20 18:22:18 +0900

    4/20

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,23 @@
 import { UserService } from './../user/user.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  NotFoundException,
+  Post,
+} from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
+import { RegisterDto } from './models/register.dto';
 
 @Controller()
 export class AuthController {
   constructor(private userService: UserService) {}
 
   @Post('register')
-  async register(@Body() body) {
+  async register(@Body() body: RegisterDto) {
+    if (body.password !== body.password_confirm) {
+      throw new BadRequestException('비밀번호가 일치하지 않음.');
+    }
     const hashed = await bcrypt.hash(body.password, 12);
     return this.userService.create({
       first_name: body.first_name,
@@ -15,5 +25,23 @@ export class AuthController {
       email: body.email,
       password: hashed,
     });
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const user = await this.userService.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (!(await bcrypt.compare(password, (await user).password))) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    return user;
   }
 }
